57fec07d09a392322ba2b56bd93d279b24a69fa4
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-09-29 17:57:04 -0500

    modified file name

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-// import ViewAllTasks from '../components/ViewAllTasks';
+import ViewAllTasks from '../components/ViewAllTasks';
 
 function Home() {
   return (
     <>
       <h2>The Repped Team Hustle List</h2>
-      {/* <ViewAllTasks /> */}
+      <ViewAllTasks />
     </>
   );
 }
