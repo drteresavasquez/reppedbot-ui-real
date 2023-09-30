fa768f15cd0dcdc9551108d204d9764917d0d714
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-09-30 13:54:43 -0500

    added title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 /* eslint-disable react/prop-types */
+import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="container">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Repped Hustle</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 
