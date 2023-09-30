a4adb676880957f6a4a9677500a98c9b13e38909
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-09-30 13:42:14 -0500

    updateded view for all tasks

--- components/ViewAllTasks.js
+++ components/ViewAllTasks.js
@@ -3,36 +3,47 @@ import { Card } from 'react-bootstrap';
 import getAllTasks from '../api/calls';
 
 export default function ViewAllTasks() {
-  const [allUserTasks, setAllUserTasks] = useState([]);
+  const [doneUserTasks, setDoneUserTasks] = useState([]);
+  const [undoneUserTasks, setUndoneUserTasks] = useState([]);
 
   useEffect(() => {
-    getAllTasks().then(setAllUserTasks);
+    getAllTasks().then((tasks) => {
+      const done = tasks.filter((t) => t.isDone);
+      const undone = tasks.filter((t) => !t.isDone);
+      setDoneUserTasks(done);
+      setUndoneUserTasks(undone);
+    });
   }, []);
   return (
     <>
       <h2>The Repped Hustle List</h2>
-      {allUserTasks.map((item) => (
+      {undoneUserTasks.map((item) => (
         <Card bg={item.isDone ? 'success' : 'dark'} key={item.firebaseKey} style={{ color: item.isDone ? '#58daa0' : 'white' }} className="mb-2">
           <Card.Body>
             <Card.Text>
-              {item.isDone ? (
-                <b>
-                  ✅{' '}
-                  <s>
-                    <i>
-                      {item.username}: {item.task}
-                    </i>
-                  </s>
-                </b>
-              ) : (
-                <span>
-                  ⬜{' '}
-                  <span style={{ color: `#${Math.floor(Math.random() * 16777215).toString(16)}` }}>
-                    <b>{item.username}:</b>
-                  </span>{' '}
-                  {item.task}
-                </span>
-              )}{' '}
+              <span>
+                ⬜{' '}
+                <span style={{ color: `#${Math.floor(Math.random() * 16777215).toString(16)}` }}>
+                  <b>{item.username}:</b>
+                </span>{' '}
+                {item.task}
+              </span>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+      {doneUserTasks.map((item) => (
+        <Card bg={item.isDone ? 'success' : 'dark'} key={item.firebaseKey} style={{ color: item.isDone ? '#58daa0' : 'white' }} className="mb-2">
+          <Card.Body>
+            <Card.Text>
+              <b>
+                ✅{' '}
+                <s>
+                  <i>
+                    {item.username}: {item.task}
+                  </i>
+                </s>
+              </b>
             </Card.Text>
           </Card.Body>
         </Card>
