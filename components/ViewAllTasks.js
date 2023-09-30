ae79f9f17da239d540f3824a1876095e5261f40a
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2023-09-30 13:13:02 -0500

    added style to view all tasks page

--- components/ViewAllTasks.js
+++ components/ViewAllTasks.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Alert } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 import getAllTasks from '../api/calls';
 
 export default function ViewAllTasks() {
@@ -9,12 +9,34 @@ export default function ViewAllTasks() {
     getAllTasks().then(setAllUserTasks);
   }, []);
   return (
-    <div>
+    <>
+      <h2>The Repped Hustle List</h2>
       {allUserTasks.map((item) => (
-        <Alert key={item.firebaseKey} variant={item.isDone ? 'danger' : 'secondary'}>
-          {item.username}: {item.task}
-        </Alert>
+        <Card bg={item.isDone ? 'success' : 'dark'} key={item.firebaseKey} style={{ color: item.isDone ? '#58daa0' : 'white' }} className="mb-2">
+          <Card.Body>
+            <Card.Text>
+              {item.isDone ? (
+                <b>
+                  ✅{' '}
+                  <s>
+                    <i>
+                      {item.username}: {item.task}
+                    </i>
+                  </s>
+                </b>
+              ) : (
+                <span>
+                  ⬜{' '}
+                  <span style={{ color: `#${Math.floor(Math.random() * 16777215).toString(16)}` }}>
+                    <b>{item.username}:</b>
+                  </span>{' '}
+                  {item.task}
+                </span>
+              )}{' '}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       ))}
-    </div>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import ViewAllTasks from '../components/ViewAllTasks';
 function Home() {
   return (
     <>
-      <h2>The Repped Team Hustle List</h2>
       <ViewAllTasks />
     </>
   );
